39b537cd1cb50fcdd396651d0e11eb41f5a9a2a3
ashima <ashimaathri@gmail.com>
2015-03-16 17:08:04 -0400

    Move initialization of audio context to pythy

--- src/lib/sound/__init__.js
+++ src/lib/sound/__init__.js
@@ -1,11 +1,3 @@
 var $builtinmodule = function() {
-  /* NOTE: The maximum number of audio contexts is 6 and it looks like everytime a program is 
-   * run it executes this file again, so, we need this check to protect against repeated
-   * context creation.
-   */
-  if(!window.__$audioContext$__) {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    window.__$audioContext$__ = new AudioContext();
-  }
   return {};
 };
